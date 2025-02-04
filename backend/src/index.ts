6fe2164b9a938b8fbe85d75a49a3d3db9a1a0db1
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-04 11:51:47 +0530

    fix bugs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'hono';
-import { sign, verify } from 'hono/jwt';
 import { userRouter } from './routes/user.routes';
 import { blogRouter } from './routes/blog.routes';
 
@@ -14,26 +13,6 @@ const app = new Hono<{
 
 app.route('/api/v1/user', userRouter)
 app.route('/api/v1/blog', blogRouter)
-
-
-// Middleware
-app.use('/api/v1/blog/*', async (c, next) => {
-    const header = c.req.header('Authorization') || '';
-    const token = header.split('')[1];
-    const response = await verify(token, c.env.JWT_SECRET);
-    if (!response.userId) {
-        return c.json(
-            {
-                message: 'Unauthorized',
-            },
-            403,
-        );
-    }
-    next();
-});
-
-
-
 
 
 export default app;
